f373fc3ec32f35eafbf6a272e2a42ee33773eaeb
OreAkintobi <oreakintobi@gmail.com>
2020-05-15 12:03:05 +0100

    updated hello.js to mycv.js

---
+++ pages/api/mycv.js
@@ -0,0 +1,229 @@
+export default (req, res) => {
+  res.status(200).json({
+    basics: {
+      name: "Ore Akintobi",
+      picture: "https://avatars0.githubusercontent.com/u/8383707?v=4",
+      label:
+        "Front End React and React Native (Cross Platform Mobile) Developer @ Decagon",
+      headline:
+        "React and React Native (Cross-Platform Mobile), JavaScript Front-End Developer. Sometimes I dabble in Kotlin & Flutter.  ",
+      summary:
+        "I am competent with Vanilla Javascript, React and React Native. I can build front end web app and mobile designs from scratch. If I can see it, I can build it. I also have experience with REST APIs and some knowledge of NodeJS and MongoDB. ",
+      website: null,
+      blog: null,
+      yearsOfExperience: 2,
+      id: "2fb2fc68-2314-482e-a74e-cb0c4f5d3955",
+      username: "OreAkintobi",
+      karma: 13,
+      email: "oreakintobi@gmail.com",
+      region: "Lagos",
+      location: {},
+      phone: null,
+      followers: 5,
+      following: 8,
+      profiles: [
+        {
+          network: "gitconnected",
+          username: "OreAkintobi",
+          url: "https://gitconnected.com/OreAkintobi",
+        },
+        {
+          network: "GitHub",
+          username: "OreAkintobi",
+          url: "https://github.com/OreAkintobi",
+        },
+      ],
+    },
+    skills: [
+      {
+        name: "React Native",
+        level: "Advanced",
+        rating: 4,
+        yearsOfExperience: 2,
+        keywords: [],
+      },
+      {
+        name: "React",
+        level: "Advanced",
+        rating: 4,
+        yearsOfExperience: 2,
+        keywords: [],
+      },
+      {
+        name: "Javascript",
+        level: "Intermediate",
+        rating: 3,
+        yearsOfExperience: 2,
+        keywords: [],
+      },
+      {
+        name: "Kotlin",
+        level: "Intermediate",
+        rating: 3,
+        yearsOfExperience: 1,
+        keywords: [],
+      },
+    ],
+    projects: [
+      {
+        name: "react-native-tracker-app-express-server",
+        displayName: "React-Native-Tracker-App-Express-Server",
+        summary:
+          "A server built for a cross platform Tracker App built here https://github.com/OreAkintobi/React-Native-Tracker-App",
+        website: "",
+        githubUrl:
+          "https://github.com/OreAkintobi/React-Native-Tracker-App-Express-Server",
+        primaryLanguage: "JavaScript",
+        languages: ["JavaScript"],
+        libraries: [],
+        images: [],
+        videos: [],
+      },
+      {
+        name: "react-native-tracker-app",
+        displayName: "React-Native-Tracker-App",
+        summary:
+          "A cross-platform (iOS + Android) app built with React Native, Expo CLI, Axios and a server I created with MongoDB & Express. That server is here https://github.com/OreAkintobi/React-Native-Tracker-App-Express-Server",
+        website: "",
+        githubUrl: "https://github.com/OreAkintobi/React-Native-Tracker-App",
+        primaryLanguage: "JavaScript",
+        languages: ["JavaScript"],
+        libraries: [],
+        images: [],
+        videos: [],
+      },
+      {
+        name: "leavemanagementsystem",
+        displayName: "LeaveManagementSystem",
+        summary:
+          "Design a platform to send/add, change/manage, delete and approve/reject leave requests. Built with Vanilla JS, jQuery and a fake JSON Rest Server",
+        website: "",
+        githubUrl: "https://github.com/OreAkintobi/LeaveManagementSystem",
+        primaryLanguage: "HTML",
+        languages: ["JavaScript", "CSS", "HTML"],
+        libraries: [],
+        images: [],
+        videos: [],
+      },
+      {
+        name: "githubfinderaftercontextapi",
+        displayName: "githubFinderAfterContextAPI",
+        summary:
+          "React App that allows the user to search for any Github user and view their profile. Built with ReactJS and the Context API",
+        website: "",
+        githubUrl: "https://github.com/OreAkintobi/githubFinderAfterContextAPI",
+        primaryLanguage: "JavaScript",
+        languages: ["CSS", "JavaScript", "HTML"],
+        libraries: [],
+        images: [],
+        videos: [],
+      },
+      {
+        name: "webdevcompanywebsite",
+        displayName: "webdevcompanywebsite",
+        summary:
+          "A Software Development Business Website built with React and MaterialUI",
+        website: "",
+        githubUrl: "https://github.com/OreAkintobi/webdevcompanywebsite",
+        primaryLanguage: "JavaScript",
+        languages: ["JavaScript", "HTML"],
+        libraries: [],
+        images: [],
+        videos: [],
+      },
+      {
+        name: "androidauthenticationapp",
+        displayName: "androidAuthenticationApp",
+        summary:
+          "Android app that makes post request to check for username and password from an authentication API and logs the user into the activity while saving user data for future entry into the app.",
+        website: "",
+        githubUrl: "https://github.com/OreAkintobi/androidAuthenticationApp",
+        primaryLanguage: "Kotlin",
+        languages: ["Kotlin"],
+        libraries: [],
+        images: [],
+        videos: [],
+      },
+      {
+        name: "blogme-app",
+        displayName: "BlogMe-App",
+        summary:
+          "This is a Blog App built with React Native, Expo CLI, Axios JSON server. JSON server is here https://github.com/OreAkintobi/BlogMe-App-JSON-Server",
+        website: "",
+        githubUrl: "https://github.com/OreAkintobi/BlogMe-App",
+        primaryLanguage: "JavaScript",
+        languages: ["JavaScript"],
+        libraries: [],
+        images: [],
+        videos: [],
+      },
+      {
+        name: "blogme-app-json-server",
+        displayName: "BlogMe-App-JSON-Server",
+        summary:
+          "This is the JSON Server for BlogMe App here https://github.com/OreAkintobi/BlogMe-App",
+        website: "",
+        githubUrl: "https://github.com/OreAkintobi/BlogMe-App-JSON-Server",
+        primaryLanguage: "",
+        languages: [],
+        libraries: [],
+        images: [],
+        videos: [],
+      },
+      {
+        name: "androidmovieviewerapp",
+        displayName: "androidMovieViewerApp",
+        summary:
+          "Created movie viewer application based on the www.themoviedb.org",
+        website: "",
+        githubUrl: "https://github.com/OreAkintobi/androidMovieViewerApp",
+        primaryLanguage: "Kotlin",
+        languages: ["Kotlin"],
+        libraries: [],
+        images: [],
+        videos: [],
+      },
+    ],
+    work: [
+      {
+        company: "Decagon",
+        position: "Mobile/Web Developer",
+        website: "https://decagonhq.com/",
+        location: "Lagos",
+        summary:
+          "I have been a front end web and mobile designer here. My job has been primarily to translate front end designs provided by the UX and Design team into working web and mobile apps.  Made use of Git for version control and built with vanilla CSS, Bootstrap, Material-UI and Styled Components design frameworks.",
+        isCurrentRole: true,
+        startDate: "2019-01-01",
+        endDate: "",
+        start: {
+          year: 2019,
+          month: 1,
+        },
+        end: {},
+        highlights: [
+          "•\tCompleting work on a hair care/salon management app.\n•\tCompleting work on a mobile app that provides support for mothers of children from 0-15 months of age.\n•\tBuilt a contact app with create, read and storage functionality.\n•\tWorked on some front end web apps for small businesses.\n•\tBuilt an authentication app that posts and gets data from an authentication API\n•\tDeveloped a movie database app that consumed RESTFUL API\n•\tMore at https://github.com/oreakintobi",
+        ],
+      },
+      {
+        company: "Freelance",
+        position: "Freelance Developer",
+        website: "",
+        location: "Remote",
+        summary:
+          "Built shopify websites, email landing pages and other projects on a freelance basis",
+        isCurrentRole: false,
+        startDate: "2018-01-01",
+        endDate: "2019-02-01",
+        start: {
+          year: 2018,
+          month: 1,
+        },
+        end: {
+          year: 2019,
+          month: 2,
+        },
+        highlights: [],
+      },
+    ],
+  });
+};
